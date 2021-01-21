f14f7b0078efa1782cd185546104f7f00f822a5a
IsaikinSergei <akinfest007@gmail.com>
2021-01-21 11:45:36 +0400

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Sergei Isaikin on 21.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
