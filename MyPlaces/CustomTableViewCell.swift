c8e373d9586ebacca5ce9d47baad9e46cacf442f
IsaikinSergei <akinfest007@gmail.com>
2021-01-28 21:08:36 +0400

    Add new ratingControl on the customTableViewCell

--- MyPlaces/CustomTableViewCell.swift
+++ MyPlaces/CustomTableViewCell.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var imageOfPlace: UIImageView! {
+        didSet {
+            imageOfPlace.layer.cornerRadius = imageOfPlace.frame.size.height / 2
+            imageOfPlace.clipsToBounds = true
+        }
+    }
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
-    
+    @IBOutlet weak var ratingControl: RatingControl! 
 }
--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -47,28 +47,20 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
             return filteredPlaces.count
         }
         
-        return places.isEmpty ? 0 : places.count
+        return places.count
     }
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-
-        var place = Place()
         
-        if isFiltering {
-            place = filteredPlaces[indexPath.row]
-        } else {
-            place = places[indexPath.row]
-        }
+        let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
 
         cell.nameLabel.text = place.name
         cell.locationLabel.text = place.location
         cell.typeLabel.text = place.type
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
-
-        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-        cell.imageOfPlace.clipsToBounds = true
+        cell.ratingControl.rating = Int(place.rating)
 
         return cell
     }
@@ -96,12 +88,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
             
-            let place: Place
-            if isFiltering {
-                place = filteredPlaces[indexPath.row]
-            } else {
-                place = places[indexPath.row]
-            }
+            let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
             
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
